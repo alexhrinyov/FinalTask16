1a0daf61dab043ccf3972e308e17bf95514803c8
alexhrinyov <hrinyou29@gmail.com>
2023-01-02 00:33:04 +0300

    Add project files.

---
+++ Calculator.Tests/CalcTests.cs
@@ -0,0 +1,34 @@
+﻿using FinalTask16;
+using NUnit.Framework;
+
+namespace Calculator.Tests
+{
+    [TestFixture]
+    public class CalcTests
+    {
+        [Test]
+        public void AdditionMustReturn3()
+        {
+            Functions fn = new Functions();
+            Assert.AreEqual(3, fn.Addition(2, 1));           
+        }
+        [Test]
+        public void SubstractionMustReturn1()
+        {
+            Functions fn = new Functions();
+            Assert.AreEqual(1, fn.Subtraction(2, 1));
+        }
+        [Test]
+        public void DivisionMustReturn6()
+        {
+            Functions fn = new Functions();
+            Assert.AreEqual(6, fn.Division(36, 6));
+        }
+        [Test]
+        public void MultMustReturn320()
+        {
+            Functions fn = new Functions();
+            Assert.AreEqual(320, fn.Miltiplication(16, 20));
+        }
+    }
+}
